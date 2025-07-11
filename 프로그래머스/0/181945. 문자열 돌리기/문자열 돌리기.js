294de4b3636c1da8295335945d812fa69f4ec047
Junyeol <junyeolkim00@gmail.com>
2025-07-11 10:06:50 +0900

    [level 0] Title: 문자열 돌리기, Time: 44.17 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -5,12 +5,12 @@ const rl = readline.createInterface({
 });
 
 let input = [];
-let str = "";
+
 rl.on('line', function (line) {
     input = [line];
 }).on('close',function(){
     str = input[0];
-    for(let i in str){
-        console.log(str[i]);
+    for(let i of str){
+        console.log(i);
     }
 });
