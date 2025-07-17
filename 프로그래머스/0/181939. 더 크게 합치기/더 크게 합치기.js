4a11f88901b1effcf376d9244e01484d9392c37f
Junyeol <junyeolkim00@gmail.com>
2025-07-17 14:11:27 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,5 +1,3 @@
 function solution(a, b) {
-    const AB = String(a) + String(b);
-    const BA = String(b) + String(a);
-    return AB >= BA ? Number(AB) : Number(BA);
+    return Math.max((`${a}${b}`),(`${b}${a}`));
 }
