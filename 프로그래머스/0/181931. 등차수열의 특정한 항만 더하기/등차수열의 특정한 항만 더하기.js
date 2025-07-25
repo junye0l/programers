6d9f65503c9fe92cdeffff1c870c3d274f25b02c
Junyeol <junyeolkim00@gmail.com>
2025-07-25 15:32:35 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,15 @@
+function solution(a, d, included) {
+    let result = 0;
+    for(let i = 0; i < included.length; i++){
+        if(included[i] === true){
+            result += a + d * i;
+        }else{
+            continue;
+        }
+    }
+    
+    return result;
+    
+    
+    
+}
