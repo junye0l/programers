1785a36f04dce85f562215b5c6b3c44aaeba7b33
Junyeol <junyeolkim00@gmail.com>
2025-08-07 17:02:24 +0900

    [level 0] Title: 배열 만들기 4, Time: 5.71 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+    let stk = [];
+    let i = 0;
+    
+    while(i < arr.length) {
+        if(stk.length === 0){
+            stk.push(arr[i]);
+            i++;
+        }else if(arr[i] > stk[stk.length - 1]){
+            stk.push(arr[i]);
+            i++;
+        }else{
+            stk.pop();
+        }
+    }
+    return stk;
+}
