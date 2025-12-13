e1f7daf71b683fd04a431f7882d65f1d319384b0
Junyeol <junyeolkim00@gmail.com>
2025-12-13 19:47:49 +0900

    [level 2] Title: 방문 길이, Time: 0.75 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -0,0 +1,40 @@
+function isValidMove(nx,ny) {
+    return nx >= -5 && nx <= 5 && ny >= -5 && ny <= 5; 
+}
+
+function updateLocation(x, y, dir) {
+    switch (dir) {
+        case "U":
+            return [x, y+1];
+        case "D":
+            return [x, y-1];
+        case "R":
+            return [x+1, y];
+        case "L":
+            return [x-1, y];
+    }
+}
+
+function solution(dirs){
+    let x = 0;
+    let y = 0;
+    
+    const visited = new Set(); // 중복 경로 제거
+    
+    for ( const dir of dirs){
+        const [nx, ny] = updateLocation(x, y, dir);
+        
+        if(!isValidMove(nx, ny)){
+            continue;
+        }
+        
+        visited.add(`${x}${y}${nx}${ny}`);
+        visited.add(`${nx}${ny}${x}${y}`);
+        
+        [x, y] = [nx, ny];
+        
+    }
+    
+    return visited.size / 2;
+    
+}
